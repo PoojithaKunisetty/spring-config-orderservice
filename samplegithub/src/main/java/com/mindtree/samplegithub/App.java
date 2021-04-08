22051ba9123a4ccf7a81a38fa1fc21a6b65ca5c9
PoojithaKunisetty <73930941+PoojithaKunisetty@users.noreply.github.com>
2021-04-08 16:18:02 +0530

    Update App.java

--- samplegithub/src/main/java/com/mindtree/samplegithub/App.java
+++ samplegithub/src/main/java/com/mindtree/samplegithub/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hi! This is my first sample project" );
+        System.out.println( "learning how to use github from eclipse" );
 
     }
 }
